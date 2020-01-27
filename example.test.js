1dd973691465b8b493c747a99cd81168be6cc51a
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-27 22:44:11 +0700

    first commit

---
+++ example.test.js
@@ -0,0 +1,8 @@
+describe('Google Test', () => {
+  it('should open google homepage', async () => {
+    jest.setTimeout(15000);
+
+    await page.goto('https://google.com');
+    await page.waitFor(5000);
+  })
+})
